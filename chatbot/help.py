9563b4cd4c04395063964f6b8af417bfcc748bec
MariaEichenlaub <m.eichenlaub@gmx.de>
2021-07-02 23:31:30 +0200

    comments

--- chatbot/help.py
+++ chatbot/help.py
@@ -16,13 +16,15 @@ def help(update, context):
     "Über die automatisch erscheinenden Buttons kannst du das gewünschte Deck ganz einfach auswählen."\
     "Im Anschluss stellt dir der Bot eine zufällige Frage aus deinem gewähltem Deck. Diese Frage kannst du nun beantworten.\n\n"\
     "<b>WICHTIG:</b> Wenn du über Telegram Web lernst, wähle zunächst die Nachricht mit der Frage aus und drücke reply."\
-    "Solltest du über die App lernen wird automatisch auf die Nachricht mir der Frage geantwortet."\
+    "Solltest du über die App lernen wird automatisch auf die Nachricht mit der Frage geantwortet."\
     "Das ist wichtig, damit der Bot überprüfen kann, ob deine Antwort richtig ist.\n\n"\
     "Im nächsten Schritt kriegst du vom Bot eine Rückmeldung ob deine Antwort richitg ist und wie die Musterlösung ist."\
     "Außerdem wirst du gefragt, ob du weiter lernen möchtest, aufhören möchtest oder das Deck wechseln möchtest. Hier kannst du wieder über die Buttons antworten."\
     "Solltest du Inhalte der Frage vom Bot nicht verstehen, lösche das Antworten auf die letzte Nachricht und tippe deine Frage ein."\
     "Der Bot antwortet dir nun auf deine Frage.\n\n"\
-    "Wenn du eine Frage zu Inhalten der Decks hast, kannst du einfach deine Frage eintippen.\n\n"\
+    "Wenn du eine Frage zu Inhalten der Decks hast, kannst du einfach deine Frage eintippen."\
+    "Sollte sich der Bot nicht sicher sein welche Frage gemeint ist, erscheinen Auswahlbuttons."\
+    "Nach Auswahl der gewünschten Frage antwortet der Bot auf die gewählte Frage.\n\n"\
     "Außerdem kannst du dir einen Timer" + u'⏱' + "stellen während du lernst. Der Timer basiert auf der Promodoro Technick."\
     "Über /timer kannst du den Timer starten. Du kannst aus vordefinierten Timern wählen oder deinen eigenen Timer einstellen."\
     "Die Auswahlmöglichkeiten erscheinen über Buttons. Bei den vordefinierten  Timern wurde eine Arbeitszeit von 25 min bzw. 50 min festgelegt."\
